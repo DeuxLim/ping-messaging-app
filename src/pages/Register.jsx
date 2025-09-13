b1cd25222a8cb04a2d46e22161f095f1f22816b8
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-13 20:47:00 +0800

    feat(authentication) : prepared register jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,27 @@
 
 export default function Register() {
   return (
-    <div>Register Page</div>
+    <div className="bg-white p-8 rounded-md shadow-2xl border-2 border-gray-200 flex flex-col gap-6">
+        <div className="flex flex-col justify-center items-center">
+            <img src="" alt="" />
+            <h1>Register</h1>
+        </div>
+        <form>
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-col">
+                    <label htmlFor="user-email">Email</label>
+                    <input type="text" id="user-email" className="border-2 border-gray-300 rounded-md"/>
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="password">password</label>
+                    <input type="password" id="password" className="border-2 border-gray-300 rounded-md"/>
+                </div>
+
+                <div className="flex flex-col justify-center items-center">
+                    <button type="submit" className="bg-green-400 py-2 px-8 rounded-md">Login</button>
+                </div>
+            </div>
+        </form>
+    </div>
   )
 }
