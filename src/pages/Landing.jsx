7ab8654766ade6c72e52cd8f23a14ea3f747c2d4
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-13 20:46:27 +0800

    refactor(ui) : minor update to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,7 @@
 export default function Landing () {
     return (
         <>
-            <h1>This is the landing page</h1>
+            <h1 className="text-2xl bg-red-100"> This is the landing page</h1>
         </>
     );
 }
