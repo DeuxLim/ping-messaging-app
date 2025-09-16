bdaa2c6da4617418781aaccbe0fd6ec60cfd7db0
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-16 19:31:58 +0800

    feature/server : setup database connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import { configDotenv } from 'dotenv';
+import connectDB from './config/database.js';
+
+// Allow .env file usage : process.env.variableName
+configDotenv();
+
+// Start Database
+connectDB();
 
 /* Initialize express */
 const app = express();
