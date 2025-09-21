6dcf4c402596763a8338d3433f319f5b1ce27b41
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-21 16:12:39 +0800

    maintenance/server/auth : authentication helper sign access and refresh token functions

---
+++ server/helpers/authHelper.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
+
+// env: ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET
+export const signAccessToken = (payload) =>
+    jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' })
+
+export const signRefreshToken = (payload) =>
+    jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: payload.rememberMe ? '30d' : '7d' })
+
+
+// hash refresh token before storing
+export const hashToken = (token) =>
+    crypto.createHash('sha256').update(token).digest('hex')
