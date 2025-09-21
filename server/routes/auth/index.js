6564b9ba9690928a4b970c9dfaa6fca224cae21f
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-21 18:07:18 +0800

    maintenance/server/auth : refresh token route

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -4,5 +4,6 @@ import AuthController from "../../controllers/auth/AuthController.js";
 const router = Router();
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
+router.post('/refresh', AuthController.refreshTokens);
 
 export default router;
