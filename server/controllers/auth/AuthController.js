50952ec8026c1bbbb1bfef47cdb03672ab3e4590
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-21 16:13:13 +0800

    maintenance/server/auth : login functionality and jwt auth usage + additional comments

--- server/controllers/auth/AuthController.js
+++ server/controllers/auth/AuthController.js
@@ -1,5 +1,7 @@
+import { hashToken, signAccessToken, signRefreshToken } from "../../helpers/authHelper.js";
 import User from "../../models/user.js";
 import authValidation from "../../validations/auth/authValidation.js";
+import jwt from 'jsonwebtoken';
 class AuthController 
 {
     constructor()
@@ -9,17 +11,19 @@ class AuthController
 
     register = async (req,res) => 
     {
+        // Validate register request
         const errors = authValidation.validateRegistration(req.body);
         if (Object.keys(errors).length > 0) {
             return res.status(400).json({ status: 400, errors });
         }
 
+        // Create user
         const newUser = await User.create(req.body);
-
         if (!newUser || !newUser._id) {
             return res.status(500).json({ message: "User was not created." });
         }
 
+        // Send back response
         res.status(201).json({
             message : "user successfully created.",
             status : 201
@@ -28,13 +32,16 @@ class AuthController
 
     login = async (req, res) =>
     {
+        // Validate login request
         const errors = authValidation.validateLogin(req.body);
         if(Object.keys(errors).length > 0){
             return res.status(400).json({ status: 400, errors });
         }
 
+        // Destructure login input request
         const { email, password, rememberMe } = req.body;
 
+        // Check login email
         let user = await User.findOne({ email });
         if(!user){
             return res.status(401).json({ 
@@ -42,6 +49,7 @@ class AuthController
             });
         }
 
+        // Check login password
         const passwordMatches = await user.comparePassword(password);
         if(!passwordMatches){
             return res.status(401).json({ 
@@ -49,8 +57,30 @@ class AuthController
             });
         }
 
-        res.status(200).json({ authenticated : true });
+        // Process JWT Access and Refresh tokens
+        const accessToken = signAccessToken({ email });
+        const refreshToken = signRefreshToken({ email, rememberMe });
+        const { exp } = jwt.decode(refreshToken);
+        const hashedRefreshToken = hashToken(refreshToken);
+
+        // Store User's refresh token
+        user.refreshToken = hashedRefreshToken;
+        user.refreshTokenExpiresAt = new Date(exp * 1000);
+        await user.save();
+
+        // Send back refresh token through httpOnly cookie
+        const ttlMs = exp * 1000 - Date.now(); // how many ms until expiration
+            res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: ttlMs
+        });
+        
+        // Send back response
+        res.status(200).json({ user, token : accessToken });
     }
+
 }
 
 export default new AuthController();
