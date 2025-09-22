c7cddf782abad798ecca203cea3bf3b327a68299
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-22 22:13:07 +0800

    feature/client/auth : protected routes authentication checking

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,7 +1,13 @@
 import { Outlet, Navigate } from "react-router";
+import useAuth from "../hooks/useAuth";
 
 export default function ProtectedRoute () {
-    const isAuthenticated = true;
+    const { user, token } = useAuth();
+
+    let isAuthenticated = false;
+    if(Object.entries(user).length > 0 && token){
+        isAuthenticated = true;
+    }
 
     return isAuthenticated ? <Outlet/> : <Navigate to="/auth/login" replace />;
 }
