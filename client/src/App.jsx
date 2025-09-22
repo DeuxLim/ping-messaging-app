001fb89b5dd527068cf5409eddd3df87f303d341
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-22 22:11:20 +0800

    feature/client/auth : wrap all routes in authprovider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { RouterProvider } from "react-router" // Enable Routing
 import { routes } from "./routes/router" // Custom Routing
+import AuthProvider from "./contexts/AuthProvider"
 
 function App() {
   return (
-    <RouterProvider router={routes} />
+    <AuthProvider>
+        <RouterProvider router={routes} />
+    </AuthProvider>
   )
 }
 
