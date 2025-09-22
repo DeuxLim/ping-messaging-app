2a0f6e7ac3d22f6e084371b6b190d07b85d70696
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-22 22:12:13 +0800

    feature/client/auth : tested AuthContext on landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,7 +1,10 @@
+import useAuth from "../hooks/useAuth.js";
+
 export default function Landing () {
+    const { user } = useAuth();
     return (
         <>
-            <h1 className="text-2xl bg-red-100"> This is the landing page</h1>
+            <h1 className="text-2xl bg-red-100"> Hello ${user.firstName} </h1>
         </>
     );
 }
