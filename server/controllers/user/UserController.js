143b779beefff93d9f359aa189300c391de49de6
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-25 19:28:08 +0800

    UserController index method

--- server/controllers/user/UserController.js
+++ server/controllers/user/UserController.js
@@ -1,9 +1,17 @@
+import User from "../../models/user.js";
+
 class UserController {
-    welcome = (req, res) => {
-        res.status(200).json({
-            "DEUX" : "WELCOME"
-        });
+
+    index = async (req, res) => {
+        try {
+            const users = await User.find().select("-refreshToken -refreshTokenExpiresAt");
+            res.status(200).json(users);
+        } catch (error) {
+            console.error("Error fetching users:", error);
+            res.status(500).json({ message: "Failed to fetch users" });
+        }
     }
+    
 }
 
 export default new UserController();
