ec244750007f422b410d81d96514474a3ee71d7b
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-26 19:56:46 +0800

    chat box header component

---
+++ client/src/components/chat/ChatBoxHeader.jsx
@@ -0,0 +1,14 @@
+
+export default function ChatBoxHeader() {
+  return (
+    <>
+    <header className="h-16 shadow-lg"> 
+        <div className="">
+            DP
+            chatmate
+            Settings
+        </div>
+    </header>
+    </>
+  )
+}
