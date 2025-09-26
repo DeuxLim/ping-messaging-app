fc465069b808c741f4e64fd8e6f7b813fa7da3a0
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-26 19:56:55 +0800

    chat content component

---
+++ client/src/components/chat/ChatContent.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function ChatContent() {
+  return (
+    <>
+     <section className="flex-1 overflow-scroll"> 
+        <div className="p-3 h-full flex flex-col gap-8">
+
+            {/* Chat inbound */}
+            <div className="flex text-sm">
+                <div className="flex gap-2 justify-center items-center max-w-[75%]">
+                    <span className="flex justify-center items-end h-full">
+                        user 
+                    </span>
+                    <div className="border-1 border-gray-400 rounded-lg px-5 py-1">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed lectus eget nulla tincidunt elementum scelerisque at elit. Sed fermentum ut massa quis elementum. Cras interdum velit nec magna ultrices, vitae tempus neque accumsan. Suspendisse lacus leo, consectetur quis tincidunt eget, cursus vel enim. Vestibulum congue eros metus, ut elementum erat tristique quis. Donec rutrum nisi sit amet molestie consequat. Nulla vestibulum sed sapien quis rutrum. Ut mattis ante sem, et interdum ex cursus sit amet. Nulla et luctus dui.
+                        Maecenas pulvinar odio justo, a convallis lorem vehicula in. Donec sagittis, lacus in imperdiet lacinia, urna lacus fringilla sem, nec placerat sem sem at nulla. In ultricies egestas enim, id aliquam sem dignissim eu. Fusce placerat sollicitudin purus a malesuada. Aliquam et tristique neque. Aenean molestie ultrices libero, sit amet convallis orci congue nec. Ut et porta turpis, vitae hendrerit nulla. Vivamus tortor tellus, porttitor nec accumsan ut, congue id lectus. Vestibulum id auctor dui. Duis gravida, nulla a volutpat imperdiet, lorem ligula suscipit nunc, eget semper nisi nisi in lorem. Donec interdum dui sagittis, luctus erat a, pharetra libero. Integer eget justo orci. Mauris nisi leo, accumsan vitae rhoncus quis, maximus et metus. Praesent ornare ornare mauris at sodales. Morbi orci leo, tincidunt vitae est at, varius vehicula lorem.
+                        In eget dignissim magna. In accumsan est sapien. Nullam ac nisi ipsum. Aliquam dapibus commodo sapien, quis pulvinar metus dapibus quis. Etiam elementum molestie fermentum. Mauris sed aliquet diam. Vestibulum mattis rutrum sapien in hendrerit. Nam pulvinar non augue dapibus fermentum. Quisque at felis laoreet, hendrerit arcu a, rutrum libero. Aliquam vel bibendum tortor. Proin tincidunt justo in facilisis ultrices.
+                        Mauris vehicula mauris lorem, eu tempor lacus ultrices vitae. Donec sit amet hendrerit augue. Proin vitae lacus ut ante tincidunt sollicitudin. Maecenas tellus mi, porta id aliquam ac, suscipit in purus. Aliquam neque ante, iaculis vel auctor sit amet, aliquam at quam. Proin imperdiet, leo ut tincidunt accumsan, nisi quam venenatis libero, sit amet placerat lorem magna in sem. Sed auctor in lectus nec consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla tempor turpis, nec scelerisque magna interdum auctor. Praesent sodales viverra ipsum, quis fringilla tortor condimentum gravida.
+                        Cras posuere nulla vehicula mauris tempus, vulputate pulvinar elit placerat. Nulla facilisi. Nunc rutrum tempor orci, eu placerat tortor elementum a. Integer scelerisque risus sem, id accumsan metus rhoncus vel. Proin id magna gravida, rhoncus nulla ac, pulvinar leo. Nam a aliquet sem. Integer turpis arcu, lacinia eu efficitur eget, tempus eget mauris. Nunc sollicitudin dapibus ex nec bibendum.
+                    </div>
+                </div>
+            </div>
+
+            {/* Chat outbound */}
+            <div className="flex text-sm justify-end pr-2.5 flex-col">
+
+                {/* Outbound messages */}
+                <div className="flex gap-2 justify-end items-end  flex-col">
+                    <div className="border-1 border-gray-400 rounded-lg px-5 py-1 max-w-[75%]">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed lectus eget nulla tincidunt elementum scelerisque at elit. Sed fermentum ut massa quis elementum. Cras interdum velit nec magna ultrices, vitae tempus neque accumsan. Suspendisse lacus leo, consectetur quis tincidunt eget, cursus vel enim. Vestibulum congue eros metus, ut elementum erat tristique quis. Donec rutrum nisi sit amet molestie consequat. Nulla vestibulum sed sapien quis rutrum. Ut mattis ante sem, et interdum ex cursus sit amet. Nulla et luctus dui.
+                        Cras posuere nulla vehicula mauris tempus, vulputate pulvinar elit placerat. Nulla facilisi. Nunc rutrum tempor orci, eu placerat tortor elementum a. Integer scelerisque risus sem, id accumsan metus rhoncus vel. Proin id magna gravida, rhoncus nulla ac, pulvinar leo. Nam a aliquet sem. Integer turpis arcu, lacinia eu efficitur eget, tempus eget mauris. Nunc sollicitudin dapibus ex nec bibendum.
+                    </div>
+                    <div className="border-1 border-gray-400 rounded-lg px-5 py-1 max-w-[75%]">
+                        Lorem ipsum dolor sit amet
+                    </div>
+                        
+                </div>
+
+                {/* Message Status : Seen, delivered, sent ? mins ago */}
+                <div className="flex justify-end items-end w-full">
+                    <span className="flex justify-center items-end h-full">
+                        user 
+                    </span>
+                    <span className="hidden">sent 1 min ago</span>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    </>
+)
+}
