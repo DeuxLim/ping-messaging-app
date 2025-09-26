01fdb26510f6e29b6cf99253901cb629809fc1bf
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-26 19:59:08 +0800

    chat input component

---
+++ client/src/components/chat/ChatInput.jsx
@@ -0,0 +1,13 @@
+
+export default function ChatInput() {
+  return (
+    <>
+    <div className="h-16 p-3"> 
+        <form action="" className="flex h-full gap-3">
+            <input type="text" placeholder="Aa" className="flex-1 p-4 rounded-xl border-1 border-gray-400"/>
+            <button type="button" className="w-16 flex items-center justify-center rounded-xl border-1 border-gray-400">Send</button>
+        </form>
+    </div>
+    </>
+  )
+}
