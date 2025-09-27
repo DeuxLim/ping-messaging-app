a4e91c839836e220eb476c0bc678149053008137
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-27 16:35:46 +0800

    sidebar search ui

--- client/src/components/chat/sidebar/SidebarSearch.jsx
+++ client/src/components/chat/sidebar/SidebarSearch.jsx
@@ -1,11 +1,22 @@
+import { TbSearch } from "react-icons/tb";
 
 export default function SidebarSearch() {
     return (
         <>
             <div className="flex flex-row p-3">
-                <div>
+                <div className="relative w-full">
+                    {/* Icon */}
+                    <div className="absolute inset-y-0 flex items-center pl-3 text-gray-500">
+                        <TbSearch />
+                    </div>
+
+                    {/* Input */}
+                    <input
+                        type="text"
+                        placeholder="Search chat..."
+                        className="w-full border border-gray-400 py-1 pl-10 rounded-full focus:outline-none"
+                    />
                 </div>
-                <input type="text" placeholder="Search chat..." className="w-full border-1 border-gray-400 py-1 px-4 rounded-full" />
             </div>
         </>
     )
