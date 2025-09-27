043e74e64f711936a2520233971f81bd565a5ff8
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-27 16:01:09 +0800

    create sidebar search  component

---
+++ client/src/components/chat/sidebar/SidebarSearch.jsx
@@ -0,0 +1,12 @@
+
+export default function SidebarSearch() {
+    return (
+        <>
+            <div className="flex flex-row p-3">
+                <div>
+                </div>
+                <input type="text" placeholder="Search chat..." className="w-full border-1 border-gray-400 py-1 px-4 rounded-full" />
+            </div>
+        </>
+    )
+}
