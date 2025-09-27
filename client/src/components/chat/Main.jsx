2c9ec1f12e822889794098304e1f6808c9493aef
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-27 22:50:40 +0800

    change component name to main

--- client/src/components/chat/Main.jsx
+++ client/src/components/chat/Main.jsx
@@ -2,10 +2,10 @@ import ChatBoxHeader from "./chat/ChatBoxHeader";
 import ChatContent from "./chat/ChatContent";
 import ChatInput from "./chat/ChatInput";
 
-export default function Chat() {
+export default function Main() {
     return (
         <>
-            <main className="flex-1 h-full">
+            <main className="flex-1 h-full shadow-xl rounded-2xl md:border-1 border-gray-300 p-2  overflow-hidden">
                 <div className="flex flex-col h-full">
 
                     {/* CHAT BOX HEADER */}
