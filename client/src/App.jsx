653ad0300f1ba074e43deb22d26e663edfd466f0
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-28 00:52:53 +0800

    organized auth context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router" // Enable Routing
 import { routes } from "./routes/router" // Custom Routing
-import AuthProvider from "./contexts/AuthProvider"
+import AuthProvider from "./contexts/auth/AuthProvider"
 
 function App() {
   return (
