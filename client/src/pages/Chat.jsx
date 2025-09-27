633c1799cd0bf4fdb91f317e2fe7da6bb31fcbc7
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-27 22:50:50 +0800

    response ui

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,16 +1,16 @@
-import Chat from "../components/chat/Chat";
+import Main from "../components/chat/Main";
 import Sidebar from "../components/chat/Sidebar";
 
 export default function Landing () {
     return (
         <>
-            <div className="h-screen flex p-3 gap-3">
+            <div className="h-screen flex md:p-3 gap-3">
 
                 {/* SIDEBAR */}
                 <Sidebar/>
                 
                 {/* MAIN */}
-                <Chat/>
+                <Main/>
 
             </div>
         </>
