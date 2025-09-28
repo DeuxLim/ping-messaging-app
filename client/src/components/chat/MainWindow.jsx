c93ffa38e688dba1aac074117177b7a15d1e2030
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-28 13:56:42 +0800

    mainwindow contents

--- client/src/components/chat/MainWindow.jsx
+++ client/src/components/chat/MainWindow.jsx
@@ -1,23 +1,14 @@
-import ChatBoxHeader from "./chat/ChatBoxHeader";
-import ChatContent from "./chat/ChatContent";
-import ChatInput from "./chat/ChatInput";
+import useChat from "../../hooks/useChat";
+import ChatWindow from "./chat/ChatWindow";
+import Start from "./start/start";
 
 export default function MainWindow() {
+    const { activeView } = useChat();
     return (
         <>
             <main className="flex-1 h-full shadow-xl rounded-2xl md:border-1 border-gray-300 p-2  overflow-hidden">
-                <div className="flex flex-col h-full">
-
-                    {/* CHAT BOX HEADER */}
-                    <ChatBoxHeader />
-
-                    {/* CHAT CONTENT */}
-                    <ChatContent />
-
-                    {/* CHAT INPUTS */}
-                    <ChatInput />
-
-                </div>
+                {activeView === "start" && <Start />}
+                {activeView === "chat" && <ChatWindow />}
             </main>
         </>
     )
