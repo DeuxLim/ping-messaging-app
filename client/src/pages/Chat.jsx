c5c592548b4d96d084b240b7ffeba2ac07f60620
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-28 11:59:15 +0800

    minor

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -10,7 +10,7 @@ export default function Landing () {
                 <Sidebar/>
                 
                 {/* MAIN */}
-                <Main/>
+                <MainWindow/>
 
             </div>
         </>
