955aa87f20830f07914ca2cce4439c541bcf2fdc
Deux Daniel Lim <limdeux27@gmail.com>
2025-09-28 13:56:21 +0800

    chatprovider initial values

--- client/src/contexts/chat/ChatProvider.jsx
+++ client/src/contexts/chat/ChatProvider.jsx
@@ -1,11 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ChatContext from "./ChatContext";
 
-export default function ChatProvider({children}){
-    const [ sidebarVisible, setSidebarVisible ] = useState(false);
+export default function ChatProvider({ children }) {
+    const [sidebarVisible, setSidebarVisible] = useState(true);
+    const [isDesktop, setIsDesktop] = useState(false);
+    const [activeView, setActiveView] = useState("start");
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(min-width: 768px)");
+
+        // set initial value
+        setIsDesktop(mediaQuery.matches);
+
+        // listen for changes
+        const handler = (e) => setIsDesktop(e.matches);
+        mediaQuery.addEventListener("change", handler);
+
+        return () => mediaQuery.removeEventListener("change", handler);
+    }, []);
+
+    const values = { 
+        sidebarVisible, 
+        setSidebarVisible, 
+        isDesktop, 
+        activeView,
+        setActiveView
+    };
 
     return (
-        <ChatContext.Provider value={{ sidebarVisible, setSidebarVisible }}>
+        <ChatContext.Provider value={ values }>
             {children}
         </ChatContext.Provider>
     )
