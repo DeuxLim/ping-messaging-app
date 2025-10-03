3d170292b24485cd94ecb1c3a2c703c406a6c621
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-03 17:14:23 +0800

    userController updated usage on userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as UserController from "../controllers/user/UserController.js";
+import UserController from "../controllers/user/UserController.js";
 
 const router = Router();
 router.get('/', UserController.index);
