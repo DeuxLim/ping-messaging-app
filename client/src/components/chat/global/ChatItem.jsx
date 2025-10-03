7f0140509d240a0faa9923cc1a08746eb0ed549b
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-03 19:53:48 +0800

    chat item initial functionality

--- client/src/components/chat/global/ChatItem.jsx
+++ client/src/components/chat/global/ChatItem.jsx
@@ -2,51 +2,67 @@ import { fetchAPI } from "../../../api/fetchApi";
 import useAuth from "../../../hooks/useAuth"
 import useChat from "../../../hooks/useChat";
 
-export default function ChatItem({ userInfo = {}, chatInfo = {}, type = "private" }) {
-    const { token } = useAuth();
+export default function ChatItem({ chatData }) {
+    const { token, currentUser } = useAuth();
     const { selectChat } = useChat();
 
-    const handleSelectChat = async ({userInfo, chatInfo, type}) => {
-        const data = {
-            type : type,
-            participants : [ userInfo ],
-            chatInfo : chatInfo
-        };
-
+    const handleSelectChat = async ({ chatData }) => {
         fetchAPI.setAuth(token);
-        const response = await fetchAPI.post("/chat", data);
 
-        if(response.error){
-            console.log(response.error);
+        let response = null;
+        if (chatData.listType === "user") {
+            response = await fetchAPI.post(`/chats`, chatData);
+            if (response?.error) {
+                console.log(response.error);
+            }
+        } else {
+            response = {
+                data : {
+                    participants : chatData.participants,
+                    chatData : chatData
+                },
+            };
         }
 
-        selectChat(response.data);
+        selectChat(response?.data);
+    }
+
+    let chatPhoto = null;
+    let chatName = "";
+    if (chatData.listType === "user") {
+        chatPhoto = chatData.profilePicture;
+        chatName = chatData.fullName;
+    } else {
+        if (chatData.isGroup) {
+            chatPhoto = chatData.chatPhoto
+        } else {
+            let chatUser = chatData.participants.filter((participant) => participant._id !== currentUser._id);
+            chatName = chatUser[0].fullName;
+        }
     }
 
     return (
         <>
             {/* Chat box */}
-            <div className="flex gap-4 items-center" onClick={() => handleSelectChat({userInfo, chatInfo, type})}>
+            <div className="flex gap-4 items-center" onClick={() => handleSelectChat({ chatData })}>
                 {/* Profile Picture */}
                 <div className="border-1 border-gray-300 flex justify-center items-center rounded-full w-15 h-15">
-                    {userInfo.profilePicture}
+                    {chatPhoto}
                 </div>
 
                 {/* Chat Details */}
                 <div className="flex-1">
                     <div className="flex justify-between">
-                        <div>{userInfo.firstName} {userInfo.lastName}</div>
-                        { chatInfo && <div>{chatInfo.time}</div> }
+                        <div>
+                            {chatName}
+                        </div>
+                        <div>{chatData.time}</div>
                     </div>
-                    {
-                        chatInfo && (
-                            <div className="flex justify-between">
-                                <div>{chatInfo.messagePreview}</div>
-                                <div>{chatInfo.status}</div>
-                            </div>
-                        )
-                    }
 
+                    <div className="flex justify-between">
+                        <div>{chatData.messagePreview}</div>
+                        <div>{chatData.status}</div>
+                    </div>
                 </div>
             </div>
         </>
