31a603ad7c5d06c842c69e87c5414afda4e1b754
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-03 15:40:52 +0800

    updated  user schema

--- server/models/user.js
+++ server/models/user.js
@@ -3,73 +3,87 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
 	{
-		firstName: { 
-			type: String, 
-			required: true 
-		},
-		lastName: { 
+		firstName: {
 			type: String,
-			required: true 
+			required: true,
 		},
-		userName: { 
+		lastName: {
 			type: String,
-			unique: true,
-			required: true
+			required: true,
 		},
-		email: { 
+		userName: {
 			type: String,
 			unique: true,
-			required: true
+			required: true,
 		},
-		password: { 
-			type: String, 
-			required: true 
+		email: {
+			type: String,
+			unique: true,
+			required: true,
 		},
-		refreshToken : { 
-			type : String, 
-			default : null 
+		password: {
+			type: String,
+			required: true,
 		},
-		refreshTokenExpiresAt : { 
-			type : Date,
-			default : null 
-		}
-	}, 
-	{ timestamps : true }
+		profilePicture: {
+			type: String, // URL to uploaded image
+			default: null,
+		},
+		bio: {
+			type: String,
+			maxlength: 150, // Short status like "Hey there! I'm using Messenger"
+			default: null,
+		},
+		isOnline: {
+			type: Boolean,
+			default: false,
+		},
+		lastSeen: {
+			type: Date,
+			default: null,
+		},
+		refreshToken: {
+			type: String,
+			default: null,
+		},
+		refreshTokenExpiresAt: {
+			type: Date,
+			default: null,
+		},
+	},
+	{ timestamps: true }
 );
 
-// Pre-save hook to hash password before saving
 userSchema.pre("save", async function (next) {
-  // only hash if password is new or modified
-  if (!this.isModified("password")) return next();
+	if (!this.isModified("password")) return next();
 
-  try {
-    const salt = await bcrypt.genSalt(10); // 10 = cost factor
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+	try {
+		const salt = await bcrypt.genSalt(10);
+		this.password = await bcrypt.hash(this.password, salt);
+		next();
+	} catch (err) {
+		next(err);
+	}
 });
 
 userSchema.set("toJSON", {
-  transform: (doc, ret) => {
-    delete ret.password;
-    delete ret.__v;
-    return ret;
-  },
+	transform: (doc, ret) => {
+		delete ret.password;
+		delete ret.__v;
+		return ret;
+	},
 });
 
 userSchema.set("toObject", {
-  transform: (doc, ret) => {
-    delete ret.password;
-    delete ret.__v;
-    return ret;
-  },
+	transform: (doc, ret) => {
+		delete ret.password;
+		delete ret.__v;
+		return ret;
+	},
 });
 
-// Add method to compare plain password with hashed one
 userSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
+	return bcrypt.compare(candidatePassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
