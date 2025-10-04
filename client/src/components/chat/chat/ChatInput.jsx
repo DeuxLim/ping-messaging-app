e77795d706e93e112254d163270b1d403647d6ac
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-04 23:22:08 +0800

    chat send message front-end initial functionality

--- client/src/components/chat/chat/ChatInput.jsx
+++ client/src/components/chat/chat/ChatInput.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
+import useChat from "../../../hooks/useChat";
+import { fetchAPI } from "../../../api/fetchApi";
+import useAuth from "../../../hooks/useAuth";
 
 export default function ChatInput() {
-  return (
-    <>
-    <div className="h-16 p-3"> 
-        <form action="" className="flex h-full gap-3">
-            <input type="text" placeholder="Aa" className="flex-1 p-4 rounded-xl border-1 border-gray-400"/>
-            <button type="button" className="w-16 flex items-center justify-center rounded-xl border-1 border-gray-400">Send</button>
-        </form>
-    </div>
-    </>
-  )
+	const [chatMessage, setChatMessage] = useState("");
+	const { currentChatData } = useChat();
+	const { token, currentUser} = useAuth();
+
+	const handleSendMessage = async (e) => {
+		e.preventDefault();
+
+		if (!chatMessage.trim()) {
+			console.log("can't send empty message...");
+		}
+
+		fetchAPI.setAuth(token);
+		const response = await fetchAPI.post(`/chats/${currentChatData.chat._id}/messages`, { message: chatMessage, sender: currentUser });
+		console.log(response);
+	}
+
+	return (
+		<>
+			<div className="h-16 p-3">
+				<form onSubmit={handleSendMessage} className="flex h-full gap-3">
+					<input
+						type="text"
+						placeholder="Aa"
+						name="chatInput"
+						className="flex-1 p-4 rounded-xl border-1 border-gray-400"
+						value={chatMessage}
+						onChange={(e) => setChatMessage(e.target.value)}
+					/>
+					<button
+						type="submit"
+						className="w-16 flex items-center justify-center rounded-xl border-1 border-gray-400"
+						disabled={!chatMessage.trim()}
+					>
+						Send
+					</button>
+				</form>
+			</div>
+		</>
+	)
 }
