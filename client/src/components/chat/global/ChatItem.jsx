43904b0ce0cbc7acffcbcc5dedaab8f794f83997
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-04 23:21:32 +0800

    minor bug fix when selecting a chat item

--- client/src/components/chat/global/ChatItem.jsx
+++ client/src/components/chat/global/ChatItem.jsx
@@ -19,7 +19,7 @@ export default function ChatItem({ chatData }) {
             response = {
                 data: {
                     participants: chatData.participants,
-                    chatData: chatData
+                    chat: chatData
                 },
             };
         }
