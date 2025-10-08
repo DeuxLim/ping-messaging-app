9342cebca3742d6d04de2210888d301527ce4916
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-08 20:23:40 +0800

    chatbox header minor update

--- client/src/components/chat/chat/ChatBoxHeader.jsx
+++ client/src/components/chat/chat/ChatBoxHeader.jsx
@@ -7,14 +7,13 @@ export default function ChatBoxHeader() {
 	const { currentChatData } = useChat();
 	const { currentUser } = useAuth();
 
-	// handle chat name logic
 	let chatName = "";
 	if (!currentChatData.isGroup) {
 		if (currentChatData.isSelfChat) {
 			const self = currentChatData.participants[0];
 			chatName = `${self.firstName} ${self.lastName}`;
 		} else {
-			const otherUser = currentChatData.participants.find(participant => participant._id !== currentUser._id);
+			const otherUser = currentChatData?.participants?.find(participant => participant._id !== currentUser._id);
 			chatName = `${otherUser?.firstName ?? ""} ${otherUser?.lastName ?? ""}`;
 		}
 	} else {
