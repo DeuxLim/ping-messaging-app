a768eacd42b9f49051e0a0e26bd61e2956e4f506
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-10 22:12:23 +0800

    used optional bg

--- client/src/pages/Auth/StartAuth.jsx
+++ client/src/pages/Auth/StartAuth.jsx
@@ -1,9 +1,21 @@
 import ping from './../../assets/images/ping-messenger-logo.png';
 import { Outlet } from 'react-router';
+import bgImage from './../../assets/images/ping-messenger-background.jpg';
 
 export default function StartAuth() {
 	return (
-		<div className="min-h-screen m-0 bg-white flex justify-center items-center">
+		<div className="min-h-screen m-0 bg-white flex justify-center items-center 
+			bg-cover 
+			bg-center 
+			bg-gradient-to-b 
+			from-black/50 
+			to-black/20"
+			style={{
+				backgroundImage: `url(${bgImage})`,
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+			}}>
+				
 			<div className='flex flex-col h-full justify-center items-center p-10 w-full'>
 
 				{/* Logo, Name, Tagline */}
@@ -18,7 +30,14 @@ export default function StartAuth() {
 				</div>
 
 				{/* Auth pages */}
-				<div className="max-w-md w-full mx-auto rounded-md flex flex-col gap-6">
+				<div className="max-w-md w-full mx-auto rounded-4xl flex flex-col gap-6 
+					md:p-10 
+					md:shadow-md 
+					md:backdrop-blur-xs 
+					md:bg-white/10 
+					md:border 
+					md:border-white/30"
+				>
 					<Outlet />
 				</div>
 
