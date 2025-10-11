a5124821b227bab7eb3b1bd90bfbcfaaf13c084d
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-11 21:03:19 +0800

    start page

--- client/src/components/chat/start/Start.jsx
+++ client/src/components/chat/start/Start.jsx
@@ -1,8 +1,18 @@
+import { IoChatbubblesOutline } from "react-icons/io5";
 
 export default function Start() {
-  return (
-    <>
-        Start a new chat
-    </>
-  )
+	return (
+		<>
+			<div className="h-full flex justify-center items-center">
+				<div className="flex justify-center items-center gap-4">
+					<div className="text-9xl text-gray-200">
+						<IoChatbubblesOutline />
+					</div>
+					<span className="text-4xl text-gray-300 font-bold">
+						Chat someone!
+					</span>
+				</div>
+			</div>
+		</>
+	)
 }
