923b3b53e6829095f4fcadf8f75d56145bee1951
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-11 14:35:45 +0800

    search chat route

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -7,5 +7,6 @@ router.get('/', ChatController.getUserChats);
 router.post('/', ChatController.findOrCreateChat);
 router.get('/:id', ChatController.getChatMessages);
 router.post('/:id/messages', ChatController.addChatMessage);
+router.get('/search/:id', ChatController.searchChat);
 
 export default router;
