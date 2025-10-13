92d4cd8bdb03f5e773c3ddb491b53e50f554b2c1
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-13 18:59:09 +0800

    separate express instantiation from server

---
+++ server/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/database.js";
+import registerRoutes from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import { configDotenv } from "dotenv";
+
+/* Allow .env file usage : process.env.variableName */
+configDotenv();
+
+/* Start Database */
+connectDB();
+
+const app = express();
+
+/* Middlewares */
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	})
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+/* Routes */
+registerRoutes(app);
+
+export default app;
--- server/server.js
+++ server/server.js
@@ -1,37 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import { configDotenv } from 'dotenv';
-import connectDB from './config/database.js';
-import registerRoutes from './routes/index.js';
-import cookieParser from 'cookie-parser';
-
-/* Allow .env file usage : process.env.variableName */
-configDotenv();
-
-/* Start Database */
-connectDB();
+import { createServer } from "http";
+import { Server } from "socket.io";
+import app from "./app.js";
 
 /* Initialize express */
-const app = express();
-
-/* Middlewares */
-app.use(cors({
-    origin : "http://localhost:5173",
-    credentials : true
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-/* Routes */
-registerRoutes(app);
-
-/* Listen for requests */
-app.listen(5001, () => {
-    console.log("✅ Backend server started...");
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+	cors: {
+		origin: "http://localhost:5173", // frontend URL
+		methods: ["GET", "POST"],
+		credentials: true,
+	},
 });
 
+/* Socket connection */
+io.on("connection", (socket) => {
+	console.log("🟢 Socket connected:", socket.id);
+});
+
+/* Listen for requests */
+httpServer.listen(5001);
+
 /* Check for errors */
-app.on('error', (err) => {
-    console.error("❌ Server error: ", err);
+httpServer.on("error", (err) => {
+	console.error("❌ Server error:", err);
 });
