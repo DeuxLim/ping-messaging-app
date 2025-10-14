7aa784a141a2082f6417dbd5291903ba8ebfec4e
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-14 15:33:47 +0800

    used socketprovider as parent on all routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { RouterProvider } from "react-router" // Enable Routing
 import { routes } from "./routes/router" // Custom Routing
 import AuthProvider from "./contexts/auth/AuthProvider"
+import SocketProvider from "./contexts/socket/SocketProvider"
 
 function App() {
-  return (
-    <AuthProvider>
-        <RouterProvider router={routes} />
-    </AuthProvider>
-  )
+	return (
+		<AuthProvider>
+			<SocketProvider>
+				<RouterProvider router={routes} />
+			</SocketProvider>
+		</AuthProvider>
+	)
 }
 
 export default App
