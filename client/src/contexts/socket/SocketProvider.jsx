b0575d8c1cd90d7844d0628c8db5a8036f2d0e8e
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-15 12:10:25 +0800

    socketprovider loading

--- client/src/contexts/socket/SocketProvider.jsx
+++ client/src/contexts/socket/SocketProvider.jsx
@@ -6,28 +6,38 @@ import useAuth from "../../hooks/useAuth";
 export default function SocketProvider({ children }) {
 	const { currentUser } = useAuth();
 	const [socket, setSocket] = useState(null);
+	const [isSocketReady, setIsSocketReady] = useState(false);
 
 	useEffect(() => {
-		if (!currentUser) {
-			return;
+		let newSocket = null;
+		try {
+			if (!currentUser || !currentUser._id) return;
+
+			newSocket = io("http://localhost:5001", { withCredentials: true });
+			setSocket(newSocket);
+
+			newSocket.emit("user:online", currentUser._id);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsSocketReady(true);
 		}
-
-		const newSocket = io("http://localhost:5001", { withCredentials: true });
-
-		newSocket.on("connect", () => {
-		});
-
-		setSocket(newSocket);
-		newSocket.emit("register", currentUser._id);
 
 		return () => {
 			newSocket.disconnect();
 		};
 	}, [currentUser]);
 
+	if (!isSocketReady) {
+		return <div>Loading...</div>;
+	}
+
+	const data = {
+		socket,
+	};
 
 	return (
-		<SocketContext.Provider value={socket}>
+		<SocketContext.Provider value={data}>
 			{children}
 		</SocketContext.Provider>
 	);
