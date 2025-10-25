67743e1ac18761699d374b23f8494a75725ea84d
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-25 19:30:45 +0800

    update profile function on usercontroller

--- server/controllers/user/UserController.js
+++ server/controllers/user/UserController.js
@@ -1,5 +1,6 @@
 import User from "../../models/user.js";
 import Chat from "../../models/chat.js";
+import cloudinary from "../../library/cloudinary.js";
 
 // GET /users - Get all users with pagination and search
 const index = async (req, res) => {
@@ -54,11 +55,52 @@ const suggested = async (req, res) => {
 		res.status(200).json(users);
 	} catch (error) {
 		console.error("Error fetching suggested users:", error);
-		res.status(500).json({ message: "Failed to fetch suggested users" });
+		res.status(500).json({ error: "Failed to fetch suggested users" });
+	}
+};
+
+const updateProfile = async (req, res) => {
+	try {
+		const currentUserId = req.user.id;
+		const { profilePicture } = req.body;
+		let updatedFields = {};
+
+		// Upload profile picture to Cloudinary and store only required data
+		if (profilePicture) {
+			const uploadResponse = await cloudinary.uploader.upload(
+				profilePicture,
+				{
+					folder: "user_profiles",
+				}
+			);
+
+			updatedFields.profilePicture = {
+				public_id: uploadResponse.public_id,
+				url: uploadResponse.secure_url,
+			};
+		}
+
+		// Return early if no valid updates
+		if (Object.keys(updatedFields).length === 0) {
+			return res.status(200).json({ updateSuccess : false, message: "no data updated." });
+		}
+
+		// Update user document
+		const user = await User.findByIdAndUpdate( currentUserId, updatedFields, {new: true} )
+			.select("-password -refreshToken -refreshAccessToken");
+
+		res.status(200).json({
+			updateSuccess: true,
+			user,
+		});
+	} catch (error) {
+		console.error("Error updating profile:", error);
+		res.status(500).json({ error });
 	}
 };
 
 export default {
 	index,
 	suggested,
+	updateProfile,
 };
