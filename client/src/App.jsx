5d0e25f5e547bf619159512dd89df484f966962f
Deux Daniel Lim <limdeux27@gmail.com>
2025-10-25 18:01:06 +0800

    renamed main entry point file to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './app.css' // Global CSS
+
 import { RouterProvider } from "react-router" // Enable Routing
 import { routes } from "./routes/router" // Custom Routing
 import AuthProvider from "./contexts/auth/AuthProvider"
 import SocketProvider from "./contexts/socket/SocketProvider"
 
-function App() {
-	return (
+createRoot(document.getElementById('root')).render(
+	<StrictMode>
 		<AuthProvider>
 			<SocketProvider>
 				<RouterProvider router={routes} />
 			</SocketProvider>
 		</AuthProvider>
-	)
-}
-
-export default App
+	</StrictMode>,
+)
