dc1f65d1dbdb6a33d0d9a2263afe27976e521445
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-01 22:44:09 +0800

    used default user img on avatar

--- client/src/components/chat/global/AvatarWithStatus.jsx
+++ client/src/components/chat/global/AvatarWithStatus.jsx
@@ -1,15 +1,11 @@
 import AvatarImage from "./AvatarImage"
 
-function AvatarWithStatus({chatPhotoUrl, userStatus}) {
+function AvatarWithStatus({ chatPhotoUrl, userStatus }) {
     return (
         <div className="relative">
             {/* Avatar Image */}
-            <div className="border border-gray-300 flex justify-center items-center rounded-full w-15 h-15 flex-shrink-0 overflow-hidden bg-gray-100 ">
-                {chatPhotoUrl ? (
-                    <AvatarImage chatPhotoUrl={chatPhotoUrl} />
-                ) : (
-                    <span className="text-gray-400 text-sm">?</span>
-                )}
+            <div className="border border-gray-300 flex justify-center items-center rounded-full w-15 h-15 flex-shrink-0 overflow-hidden bg-gray-100">
+                <AvatarImage chatPhotoUrl={chatPhotoUrl} />
             </div>
 
             {/* Status Icon */}
