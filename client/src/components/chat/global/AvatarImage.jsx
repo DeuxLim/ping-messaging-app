5f24245a76eef48fb581b3f78b1ad79192a241ba
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-01 22:44:35 +0800

    fallback photo for avatarImage component

--- client/src/components/chat/global/AvatarImage.jsx
+++ client/src/components/chat/global/AvatarImage.jsx
@@ -1,10 +1,14 @@
+import defaultUserImage from "../../../assets/images/default-user.png"
+export default function AvatarImage({ chatPhotoUrl }) {
 
-export default function AvatarImage({chatPhotoUrl}) {
-    return (
-        <img
-            src={chatPhotoUrl}
-            alt="Chat avatar"
-            className="w-full h-full object-cover"
-        />
-    )
+  return (
+    <img
+      src={chatPhotoUrl || defaultUserImage}
+      alt="Chat avatar"
+      onError={(e) => {
+        e.currentTarget.src = defaultUserImage; // fallback if broken URL
+      }}
+      className="w-full h-full object-cover"
+    />
+  );
 }
