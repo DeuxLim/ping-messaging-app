8c7f94bf1365698939c73cc57bb9f192bd9d34ab
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-03 20:09:54 +0800

    clickable icon to navigate to create chat

--- client/src/components/chat/sidebar/SidebarHeader.jsx
+++ client/src/components/chat/sidebar/SidebarHeader.jsx
@@ -2,6 +2,7 @@ import useAuth from "../../../hooks/useAuth.js";
 import { Link } from "react-router"
 import AvatarWithStatus from "../global/AvatarWithStatus.jsx";
 import useChat from "../../../hooks/useChat.js";
+import { FaRegEdit } from "react-icons/fa";
 
 export default function SidebarHeader() {
 	const { currentUser } = useAuth();
@@ -22,6 +23,12 @@ export default function SidebarHeader() {
 						<div className="flex justify-center items-center text-sm">
 							{currentUser.firstName} {currentUser.lastName} <br />
 							{currentUser.email}
+						</div>
+					</Link>
+
+					<Link to="/chats/create-new-chat" className="flex justify-center items-center px-3">
+						<div className="text-2xl">
+							<FaRegEdit />
 						</div>
 					</Link>
 				</div>
