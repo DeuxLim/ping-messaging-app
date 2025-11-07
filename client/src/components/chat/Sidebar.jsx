da8b52b94a53366c9c1aa92cb84864200d0baac3
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-07 16:49:46 +0800

    resizeable sidebar

--- client/src/components/chat/Sidebar.jsx
+++ client/src/components/chat/Sidebar.jsx
@@ -8,7 +8,7 @@ export default function Sidebar() {
     return (
         <>
             {/* SIDEBAR */}
-            <aside className="flex md:w-88 w-full shadow-xl h-full flex-col border-r border-gray-200">
+            <aside className="flex flex-col h-full border-r border-gray-200 shadow-xl resize-x overflow-auto md:w-88 w-full min-w-[250px] max-w-[500px]">
 
                 {/* HEADER */}
                 <SidebarHeader />
