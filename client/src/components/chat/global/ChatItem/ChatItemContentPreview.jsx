ca4d0e37e0f6417cf6c73ccb4d5a0f50c1351b12
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-09 20:25:52 +0800

    Create ChatItemContentPreview component

---
+++ client/src/components/chat/global/ChatItem/ChatItemContentPreview.jsx
@@ -0,0 +1,42 @@
+
+export default function ChatItemContentPreview({data}) {
+    const {
+        typingChats,
+        chatData, 
+        isLastMsgSeen, 
+        currentUser, 
+        unread, 
+        lastMessageSender, 
+        lastMessageDateTime 
+    } = data;
+
+    return (
+        <>
+            {typingChats[chatData._id] ? (
+                <div className="text-xs text-gray-600">
+                    Typing ...
+                </div>
+            ) : (
+                <div className="flex gap-1">
+                    <div className="flex items-center gap-2 text-xs text-gray-600 min-w-0">
+                        <span className={`truncate flex-1 ${!isLastMsgSeen && chatData.lastMessage?.sender?._id !== currentUser._id ? "font-bold" : ""}`}>
+                            {chatData.lastMessage?.sender?._id === currentUser._id ? (
+                                `you: ${chatData.lastMessage?.text || ""}`
+                            ) : chatData.unreadCount > 0 && !isLastMsgSeen ? (
+                                unread
+                            ) : !isLastMsgSeen ? (
+                                `${lastMessageSender} sent a message`
+                            ) : (
+                                chatData.lastMessage?.text || ""
+                            )}
+                        </span>
+
+                    </div>
+                    <span className="text-gray-500 whitespace-nowrap flex-shrink-0 text-xs">
+                        • {lastMessageDateTime}
+                    </span>
+                </div>
+            )}
+        </>
+    )
+}
