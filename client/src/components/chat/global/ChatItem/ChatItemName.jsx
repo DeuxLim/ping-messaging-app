0588f91087e2eba4641f82ebcf250a6d1a792485
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-11 15:57:52 +0800

    minor ui fix

--- client/src/components/chat/global/ChatItem/ChatItemName.jsx
+++ client/src/components/chat/global/ChatItem/ChatItemName.jsx
@@ -1,11 +1,11 @@
 
-export default function ChatItemName({data}) {
-    const {isLastMsgSeen, chatData, currentUser, chatName} = data;
+export default function ChatItemName({ data }) {
+    const { isLastMsgSeen, chatData, currentUser, chatName, existingChat } = data;
 
     return (
         <div className="flex items-center gap-2">
             <span
-                className={`truncate ${!isLastMsgSeen && chatData.lastMessage?.sender?._id !== currentUser._id ? "font-semibold" : ""}`}
+                className={`truncate ${!isLastMsgSeen && chatData.lastMessage?.sender?._id !== currentUser._id && existingChat ? "font-semibold" : ""}`}
             >
                 {chatName}
             </span>
