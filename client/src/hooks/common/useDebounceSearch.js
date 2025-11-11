adb1931123ae2628c58553f643b496a5c0c3507a
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-11 16:07:35 +0800

    new reusable hook for search debounce

---
+++ client/src/hooks/common/useDebounceSearch.js
@@ -0,0 +1,17 @@
+import React, { useRef, useState } from "react";
+
+export default function useDebounceSearch(callback, delay = 400) {
+	const [query, setQuery] = useState("");
+	const debounceRef = useRef(null);
+
+	/* Debounce, only trigger search requests after a few milliseconds. (not per key stroke) */
+	const handleChange = (e) => {
+		const value = e.target.value;
+		setQuery(value);
+
+		clearTimeout(debounceRef.current);
+		debounceRef.current = setTimeout(() => callback(value), delay);
+	};
+
+	return { query, handleChange };
+}
