cd596b602232732e1676a6286f3aa3851a1213ef
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-13 13:50:49 +0800

    start page background color

--- client/src/components/chat/start/Start.jsx
+++ client/src/components/chat/start/Start.jsx
@@ -3,7 +3,7 @@ import { FaFacebookMessenger } from "react-icons/fa6";
 export default function Start() {
 	return (
 		<>
-			<div className="h-full flex justify-center items-center bg-gray-100">
+			<div className="h-full flex justify-center items-center bg-white">
 				<div className="flex justify-center items-center gap-4">
 					<div className="text-4xl text-gray-400">
 						<FaFacebookMessenger />
