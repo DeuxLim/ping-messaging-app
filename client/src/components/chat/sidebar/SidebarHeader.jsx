1a6c969be999d393cdfb00e56e1d23aeca177205
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-13 10:30:00 +0800

    sidebar header ui update and cleanup

--- client/src/components/chat/sidebar/SidebarHeader.jsx
+++ client/src/components/chat/sidebar/SidebarHeader.jsx
@@ -5,28 +5,13 @@ import { RiEditBoxLine } from "react-icons/ri";
 // import useChat from "../../../hooks/useChat.js";
 
 export default function SidebarHeader() {
-	// const { currentUser } = useAuth();
-	// const { isUserOnline } = useChat();
-
-	// const photoUrl = currentUser.profilePicture?.url || "";
-	// const userStatus = isUserOnline(currentUser._id) ? "online" : "offline";
-
 	return (
 		<>
 			<header className="flex">
-				<div className="flex justify-between w-full items-center px-4 py-2">
-					{/* <Link to="/profile" className="flex gap-4 justify-start p-3 rounded-md hover:bg-gray-100 cursor-pointer">
-						<AvatarWithStatus chatPhotoUrl={photoUrl} userStatus={userStatus} />
-						<div className="flex justify-center items-center text-sm">
-							{currentUser.firstName} {currentUser.lastName} <br />
-							{currentUser.email}
-						</div>
-					</Link> */}
-					<h1 className='text-xl font-bold text-blue-600'>messenger</h1>
-
-
-					<Link to="/chats/create-new-chat" className="flex justify-center items-center pl-3">
-						<div className="text-xl">
+				<div className="flex justify-between w-full items-center px-5 py-2">
+					<h1 className='text-2xl font-semibold'>Chats</h1>
+					<Link to="/chats/create-new-chat" className="flex justify-center items-center p-2 rounded-full bg-gray-100">
+						<div className="text-xl flex justify-center items-center">
 							<RiEditBoxLine />
 						</div>
 					</Link>
