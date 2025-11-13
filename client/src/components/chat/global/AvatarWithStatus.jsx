cdd748c9f6f99abc41db68fe546f34aa02145e35
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-13 13:50:24 +0800

    active indicator ui update

--- client/src/components/chat/global/AvatarWithStatus.jsx
+++ client/src/components/chat/global/AvatarWithStatus.jsx
@@ -1,18 +1,18 @@
 import AvatarImage from "./AvatarImage"
 
-function AvatarWithStatus({ chatPhotoUrl, userStatus }) {
+function AvatarWithStatus({ chatPhotoUrl, userStatus, containerClass }) {
     return (
-        <div className="relative">
+        <div className={`relative ${containerClass}`}>
             {/* Avatar Image */}
-            <div className="border border-gray-300 flex justify-center items-center rounded-full size-12 flex-shrink-0 overflow-hidden bg-gray-100">
+            <div className="border border-gray-300 flex justify-center items-center rounded-full h-full w-full flex-shrink-0 overflow-hidden bg-gray-100">
                 <AvatarImage chatPhotoUrl={chatPhotoUrl} />
             </div>
 
             {/* Status Icon */}
             {
                 userStatus === "online" && (
-                    <div className="absolute right-0 bottom-1">
-                        <div className="h-4 w-4 rounded-full bg-green-500"></div>
+                    <div className="absolute right-0 bottom-0">
+                        <div className="size-3.5 border-2 border-white rounded-full bg-green-500"></div>
                     </div>
                 )
             }
