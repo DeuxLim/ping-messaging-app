94d990b4a5b1ae10725cac73a0bf7e7d2efb4c73
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-14 18:39:46 +0800

    new chat updated route

--- client/src/components/chat/sidebar/SidebarHeader.jsx
+++ client/src/components/chat/sidebar/SidebarHeader.jsx
@@ -7,7 +7,7 @@ export default function SidebarHeader() {
 			<header className="flex">
 				<div className="flex justify-between w-full items-center px-5 py-2">
 					<h1 className='text-2xl font-semibold'>Chats</h1>
-					<Link to="/chats/create-new-chat" className="flex justify-center items-center p-2 rounded-full bg-gray-100">
+					<Link to="/chats/new" className="flex justify-center items-center p-2 rounded-full bg-gray-100">
 						<div className="text-xl flex justify-center items-center">
 							<RiEditBoxLine />
 						</div>
