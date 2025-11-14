59f52e95d790a4f86204c1850b55f1e71d1d94fa
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-14 19:21:51 +0800

    minor update on activechat provider

--- client/src/contexts/chat/ActiveChat/ActiveChatProvider.jsx
+++ client/src/contexts/chat/ActiveChat/ActiveChatProvider.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import ActiveChatContext from "./ActiveChatContext";
 
 export default function ActiveChatProvider({ children }) {
+	const [filteredList, setFilteredList] = useState([]);
 
-    const data = {
-
-    };
+	const data = {
+		filteredList, setFilteredList
+	};
 
 	return (
 		<ActiveChatContext.Provider value={data}>
