93fa5338aa8b5aacf60ff0aa4c79d5e0eb3c9b2c
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-14 19:22:26 +0800

    minor update

--- client/src/components/chat/chat/NewChatLayout.jsx
+++ client/src/components/chat/chat/NewChatLayout.jsx
@@ -1,17 +1,18 @@
-import { Link, Outlet } from "react-router";
+import { Outlet } from "react-router";
 import useChat from "../../../hooks/useChat"
 import ChatItem from "../global/ChatItem";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo } from "react";
 import { isEmpty } from "../../../utilities/utils";
 import ChatSearchInput from "./ChatSearchInput";
+import useActiveChat from "../../../hooks/useActiveChat";
 
 export default function NewChatLayout() {
-	const { usersAndChatsList } = useChat();
-	const [filteredList, setFilteredList] = useState([]);
+	const { usersAndChatsList, activeChatData } = useChat();
+	const { setFilteredList, filteredList } = useActiveChat();
 
 	useEffect(() => {
 		setFilteredList(usersAndChatsList);
-	}, [usersAndChatsList]);
+	}, [usersAndChatsList, setFilteredList]);
 
 	const chatNodes = useMemo(() => {
 		if (isEmpty(filteredList)) return [];
@@ -28,14 +29,15 @@ export default function NewChatLayout() {
 			? <div className="text-gray-500">No results</div>
 			: chatNodes;
 
-
-	console.log(renderList);
-
 	return (
 		<>
 			<div>
-				<ChatSearchInput/>
-				<Outlet />
+				<ChatSearchInput />
+				{renderList}
+
+				{activeChatData && (
+					<Outlet />
+				)}
 			</div>
 		</>
 	)
