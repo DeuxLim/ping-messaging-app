498634f843b3dd94c2b7ed20d10ca49f79d87f1d
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-24 20:18:25 +0800

    new chat layout initial functionality

--- client/src/components/chat/chat/NewChatLayout.jsx
+++ client/src/components/chat/chat/NewChatLayout.jsx
@@ -1,42 +1,78 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import useChat from "../../../hooks/useChat"
 import ChatItem from "../global/ChatItem";
-import { useEffect, useMemo } from "react";
+import { useEffect, useState } from "react";
 import { isEmpty } from "../../../utilities/utils";
 import ChatSearchInput from "./ChatSearchInput";
 import useActiveChat from "../../../hooks/useActiveChat";
+import { fetchAPI } from "../../../api/fetchApi";
+import useAuth from "../../../hooks/useAuth";
 
 export default function NewChatLayout() {
-	const { usersAndChatsList, activeChatData } = useChat();
-	const { setFilteredList, filteredList } = useActiveChat();
+	const { token } = useAuth();
+	const { usersAndChatsList, activeChatData, clearActiveChat, setActiveChat, setActiveChatMessages } = useChat();
+	const { setFilteredList, selectedChats, setSelectedChats } = useActiveChat();
+	const [isLoading, setIsLoading] = useState(false);
+	const navigate = useNavigate();
 
 	useEffect(() => {
+		clearActiveChat();
 		setFilteredList(usersAndChatsList);
-	}, [usersAndChatsList, setFilteredList]);
 
-	const chatNodes = useMemo(() => {
-		if (isEmpty(filteredList)) return [];
+		let isMounted = true;
 
-		return filteredList?.map(item => (
-			<div key={`${item.type}-${item._id}`} className="text-sm relative after:content-[''] after:absolute after:bottom-0 after:right-0 after:w-[94%] after:border-b after:border-gray-200">
-				<ChatItem chatData={item} variant="compact" isSelecting={true} />
+		const loadChat = async () => {
+			try {
+				setIsLoading(true);
+				// find chat or user entry by ID
+				const chatData = usersAndChatsList.find((item) => item._id === selectedChats[0]?._id);
+
+				fetchAPI.setAuth(token);
+				const res = await fetchAPI.get(`/chats/${selectedChats[0]?._id}`);
+				if (res?.error) setActiveChatMessages([]);
+				else setActiveChatMessages(res);
+
+				if (isMounted) {
+					setActiveChat(chatData);
+				}
+			} catch (err) {
+				console.error("ChatLayout error:", err);
+				if (isMounted) navigate("/chats", { replace: true });
+			} finally {
+				if (isMounted) setIsLoading(false);
+			}
+		};
+
+		if (!isEmpty(selectedChats)) {
+			loadChat();
+		}
+	}, [usersAndChatsList, setFilteredList, selectedChats, clearActiveChat, navigate, setActiveChat, setActiveChatMessages, token]);
+
+	// cleanup only
+	useEffect(() => {
+		return () => {
+			setSelectedChats([]);
+			clearActiveChat();
+		};
+	}, [setSelectedChats, clearActiveChat]);
+
+	if (isLoading) {
+		return (
+			<div className="flex items-center justify-center h-full text-gray-500 text-sm">
+				Loading chat data...
 			</div>
-		));
-	}, [filteredList]);
-
-	const renderList =
-		usersAndChatsList.length === 0
-			? <div className="text-gray-500">No results</div>
-			: chatNodes;
+		);
+	}
 
 	return (
 		<>
-			<div>
+			<div className="relative flex flex-col h-full">
 				<ChatSearchInput />
-				{renderList}
 
 				{activeChatData && (
-					<Outlet />
+					<div className="h-full flex-1 overflow-y-auto hide-scrollbar">
+						<Outlet />
+					</div>
 				)}
 			</div>
 		</>
