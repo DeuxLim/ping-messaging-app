623178a3d9a6890a1c3fafe2aa0f383681f1232f
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-27 22:42:22 +0800

    ChatDetailsPanel functionality

--- client/src/components/chat/chat/ChatDetailsPanel.jsx
+++ client/src/components/chat/chat/ChatDetailsPanel.jsx
@@ -1,47 +1,75 @@
 import useAuth from '../../../hooks/useAuth';
 import useChat from '../../../hooks/useChat';
-import { getOtherParticipant } from '../../../utilities/utils';
+import { getOtherParticipant, getOtherParticipants } from '../../../utilities/utils';
 import AvatarImage from '../global/AvatarImage';
 
 export default function ChatDetailsPanel() {
     const { activeChatData } = useChat();
     const { currentUser } = useAuth();
 
-    const isGroup = !!activeChatData.isGroup;
+    const isGroup = activeChatData?.isGroup;
     const otherUser = !isGroup
-        ? getOtherParticipant(activeChatData.participants, currentUser?._id)
-        : null;
+        ? getOtherParticipant(activeChatData?.participants, currentUser?._id)
+        : getOtherParticipants(activeChatData?.participants, currentUser?._id);
 
     return (
-        <div className="flex justify-center items-center mt-4 flex-col gap-4">
-            {activeChatData.participants.map((p) => {
-                if (!isGroup && p._id === currentUser._id) return;
+        <div className="flex justify-center items-center mt-4 flex-col gap-4 ">
+            <div className='flex flex-col justify-center items-center'>
 
-                return (
-                    <div className="size-40 rounded-full overflow-hidden" key={`${p._id}`}>
-                        <AvatarImage chatPhotoUrl={p?.profilePicture?.url} />
-                    </div>
-                );
-            })}
+                {/* Display Photo */}
+                <div className={`flex justify-center items-center w-58 min-h-28 relative ${!isGroup && 'h-44'}`}>
+                    {activeChatData?.participants?.map((p, index) => {
+                        if (p._id === currentUser._id) return;
 
+                        const displayPhotos = isGroup ? (
+                            <div
+                                key={p?._id}
+                                className={`absolute ${index === 0 ? 'top-8 right-20' : 'top-12 left-20'}`}
+                            >
+                                <div className="size-12 rounded-full overflow-hidden">
+                                    <AvatarImage chatPhotoUrl={p?.profilePicture?.url} />
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="size-40 rounded-full overflow-hidden" key={`${p._id}`}>
+                                <AvatarImage chatPhotoUrl={p?.profilePicture?.url} />
+                            </div>
+                        );
 
-            {!isGroup && (
+                        return displayPhotos;
+                    })}
+                </div>
+
+                {/* Display Names */}
                 <div className="flex flex-col justify-center items-center gap-2">
                     <div className="flex flex-col justify-center items-center">
-                        <div className="font-semibold text-2xl">
-                            {otherUser.fullName}
-                        </div>
+                        {isGroup ? (
+                            <div className="font-semibold text-md">
+                                {otherUser.map(u => u.firstName).join(", ")}
+                            </div>
+                        ) : (
+                            <div className="font-semibold text-2xl">
+                                {otherUser.fullName}
+                            </div>
+                        )}
+                    </div>
+
+                    {!isGroup && (
                         <div>
-                            @{otherUser.userName}
+                            @{otherUser?.userName}
                         </div>
-                    </div>
-                    <div className="flex justify-center items-center">
-                        <button type="button" className="py-2 px-2 text-xs bg-gray-300 flex justify-center items-center rounded-full font-semibold">
-                            View Profile
-                        </button>
-                    </div>
+                    )}
+
+                    {!isGroup && (
+                        <div className="flex justify-center items-center">
+                            <button type="button" className="py-2 px-2 text-xs bg-gray-300 flex justify-center items-center rounded-full font-semibold">
+                                View Profile
+                            </button>
+                        </div>
+                    )}
                 </div>
-            )}
-        </div>
+
+            </div>
+        </div >
     )
 }
