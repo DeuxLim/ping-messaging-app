efe3dedbd9673929e813f2a84bfd3c4ef66452bf
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-27 22:42:11 +0800

    sidebar chat new chat preview

--- client/src/components/chat/sidebar/SidebarChats.jsx
+++ client/src/components/chat/sidebar/SidebarChats.jsx
@@ -2,9 +2,11 @@ import ChatItem from "../global/ChatItem";
 import useChat from "../../../hooks/useChat";
 import CenteredMessage from "../../common/CenteredMessage";
 import { useMemo } from "react";
+import useActiveChat from "../../../hooks/useActiveChat";
 
 export default function SidebarChats() {
-    const { usersAndChatsList, searchResults, isSearch = false, isLoading = false, error = null } = useChat();
+    const { usersAndChatsList, searchResults, isSearch = false, isLoading = false, error = null, activeChatData } = useChat();
+    const { selectedChats } = useActiveChat();
 
     const chatNodes = useMemo(() => {
         const filtered = isSearch
@@ -16,6 +18,10 @@ export default function SidebarChats() {
         ));
     }, [usersAndChatsList, isSearch, searchResults]);
 
+    const tempChatPreview = selectedChats.length > 1 && (
+        <ChatItem key={activeChatData?._id} chatData={activeChatData} variant={"preview"} />
+    )
+
     const renderList = usersAndChatsList.length === 0 ? <div className="text-gray-500">No results</div> : chatNodes;
 
     if (isLoading) return <CenteredMessage>Loading...</CenteredMessage>;
@@ -25,6 +31,7 @@ export default function SidebarChats() {
         <section className="flex-1 px-2 py-3 flex flex-col gap-3 overflow-auto">
             <div>
                 <div className="text-sm flex flex-col gap-1">
+                    {tempChatPreview}
                     {renderList}
                 </div>
             </div>
