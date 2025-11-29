d6bdbdc760b3a05dfdb429e1af27132c82630c72
Deux Daniel Lim <limdeux27@gmail.com>
2025-11-29 19:26:34 +0800

    updated message schema to handle multiple media attachments

--- server/models/message.js
+++ server/models/message.js
@@ -1,35 +1,41 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
-    {
-        chat: { 
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: "Chat",
-            required: true
-        },
-        sender: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        text: { 
-            type: String
-        },
-        mediaUrl: { 
-            type: String,
-            default: null 
-        },
-        mediaType: {
-            type: String,
-            enum: ["image", "video", "file"],
-            default: null,
-        },
-        isSeen : {
-            type : Boolean,
-            default : false
-        }
-    }, 
-    { timestamps: true }
+	{
+		chat: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Chat",
+			required: true,
+		},
+		sender: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		text: {
+			type: String,
+			default: "",
+		},
+
+		// multiple media support
+		media: [
+			{
+				url: { type: String, required: true },
+				publicId : String, 
+				type: {
+					type: String,
+					enum: ["image", "video", "file"],
+					required: true,
+				},
+			},
+		],
+
+		isSeen: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true }
 );
 
 const Message = mongoose.model("Message", messageSchema);
