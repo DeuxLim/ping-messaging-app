418cea89f20462a77948ee9f2d69ed83c6dbc59f
Deux Daniel Lim <limdeux27@gmail.com>
2025-12-18 19:20:21 +0800

    icon menu usage

--- client/src/pages/ChatApp.jsx
+++ client/src/pages/ChatApp.jsx
@@ -2,6 +2,7 @@ import { Outlet, useLocation } from "react-router";
 import Sidebar from "../components/chat/Sidebar";
 import useChatDisplay from "../hooks/useChatDisplay";
 import ChatSettings from "../components/chat/chat/ChatSettings";
+import IconMenu from "../components/chat/IconMenu/IconMenu";
 
 export default function ChatApp() {
     const { sidebarVisible, isDesktop, isChatSettingsOpen } = useChatDisplay();
@@ -10,7 +11,9 @@ export default function ChatApp() {
 
     return (
         <>
-            <div className="h-screen flex p-4 bg-gray-100 gap-4">
+            <div className="h-screen flex py-4 px-2 bg-gray-100 gap-6">
+                <IconMenu />
+
                 {isDesktop
                     ? (
                         <>
