0e232ca1427cd44ed9f7ba288d416100e7d52bf6
Deux Daniel Lim <limdeux27@gmail.com>
2026-01-03 08:44:51 +0800

    updated chat schema, added nickname field

--- server/models/chat.js
+++ server/models/chat.js
@@ -1,26 +1,31 @@
 import mongoose from "mongoose";
 
 const chatSchema = new mongoose.Schema(
-    {
-        isGroup : {
-            type : Boolean,
-            default : false
-        },
-        participants : [
-            {
-                type : mongoose.Schema.Types.ObjectId,
-                ref :  "User"
-            }
-        ],
-        chatName : {
-            type : String
-        },
-        lastMessage : {
-            type : mongoose.Schema.Types.ObjectId, 
-            ref : "Message"
-        }
-    }, 
-    { timestamps : true } 
+	{
+		isGroup: {
+			type: Boolean,
+			default: false,
+		},
+		participants: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+		nicknames: {
+			type: Map,
+			of: String,
+			default: {},
+		},
+		chatName: {
+			type: String,
+		},
+		lastMessage: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Message",
+		},
+	},
+	{ timestamps: true }
 );
 
 const Chat = mongoose.model("Chat", chatSchema);
