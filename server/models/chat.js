21b863a0ca325f170dd13f12b3c4de8625df4014
Deux Daniel Lim <limdeux27@gmail.com>
2026-01-05 17:18:55 +0800

    updated chat schema - additional chat meta data

--- server/models/chat.js
+++ server/models/chat.js
@@ -5,28 +5,77 @@ const chatSchema = new mongoose.Schema(
 		isGroup: {
 			type: Boolean,
 			default: false,
+			index: true,
 		},
+
 		participants: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+				required: true,
+			},
+		],
+
+		// Group chat only
+		chatName: {
+			type: String,
+			trim: true,
+			maxlength: 50,
+		},
+
+		chatPhoto: {
+			type: String, // URL
+		},
+
+		admins: [
 			{
 				type: mongoose.Schema.Types.ObjectId,
 				ref: "User",
 			},
 		],
+
+		// Per-user customization
 		nicknames: {
 			type: Map,
-			of: String,
+			of: String, // userId -> nickname
 			default: {},
 		},
-		chatName: {
-			type: String,
-		},
+
 		lastMessage: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: "Message",
+		},
+
+		// Per-user chat state
+		mutedBy: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+
+		archivedBy: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+
+		// Soft delete (do NOT hard delete chats)
+		deletedFor: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+
+		// Audit
+		updatedBy: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
 		},
 	},
 	{ timestamps: true }
 );
 
-const Chat = mongoose.model("Chat", chatSchema);
-export default Chat;
+export default mongoose.model("Chat", chatSchema);
