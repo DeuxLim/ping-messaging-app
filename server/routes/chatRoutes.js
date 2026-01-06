b8c42d36aa5767ef24a49a1a407befa4d90e915d
Deux Daniel Lim <limdeux27@gmail.com>
2026-01-06 09:10:10 +0800

    updateChat route

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -8,5 +8,6 @@ router.post('/', ChatController.findOrCreateChat);
 router.get('/search', ChatController.searchChat);
 router.get('/:id', ChatController.getChatMessages);
 router.post('/:id/messages', ChatController.addChatMessage);
+router.patch('/:id', ChatController.updateChat);
 
 export default router;
