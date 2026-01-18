d725a0f41b480c582c7430d317f1bb8f254d66fd
Deux Daniel Lim <limdeux27@gmail.com>
2026-01-18 10:08:26 +0800

    cleaned up refresh tokens and config file

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,4 +1,26 @@
 // config/auth.js
-export const VERIFICATION_TOKEN_TTL = Number(
-	process.env.VERIFICATION_TOKEN_TTL
+const requireEnv = (key) => {
+	if (!process.env[key]) {
+		throw new Error(`Missing env var: ${key}`);
+	}
+	return process.env[key];
+};
+
+const parseNumber = (key) => {
+	const value = Number(requireEnv(key));
+	if (Number.isNaN(value)) {
+		throw new Error(`${key} must be a number`);
+	}
+	return value;
+};
+
+export const VERIFICATION_TOKEN_TTL = parseNumber("VERIFICATION_TOKEN_TTL");
+
+export const ACCESS_TOKEN_TTL = requireEnv("ACCESS_TOKEN_TTL");
+export const ACCESS_TOKEN_SECRET = requireEnv("ACCESS_TOKEN_SECRET");
+
+export const REFRESH_TOKEN_TTL = requireEnv("REFRESH_TOKEN_TTL");
+export const REFRESH_TOKEN_SECRET = requireEnv("REFRESH_TOKEN_SECRET");
+export const REFRESH_TOKEN_REMEMBER_TTL = requireEnv(
+	"REFRESH_TOKEN_REMEMBER_TTL"
 );
--- server/helpers/authHelper.js
+++ server/helpers/authHelper.js
@@ -1,14 +1,24 @@
-import jwt from 'jsonwebtoken'
-import crypto from 'crypto'
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import {
+    ACCESS_TOKEN_SECRET,
+	ACCESS_TOKEN_TTL,
+	REFRESH_TOKEN_SECRET,
+	REFRESH_TOKEN_TTL,
+	REFRESH_TOKEN_REMEMBER_TTL,
+} from "../config/auth.js";
 
 // env: ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET
 export const signAccessToken = (payload) =>
-    jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' })
+	jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+		expiresIn: ACCESS_TOKEN_TTL,
+	});
 
-export const signRefreshToken = (payload, rememberMe = false ) =>
-    jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: rememberMe ? '30d' : '7d' })
-
+export const signRefreshToken = (payload, rememberMe = false) =>
+	jwt.sign(payload, REFRESH_TOKEN_SECRET, {
+		expiresIn: rememberMe ? REFRESH_TOKEN_REMEMBER_TTL : REFRESH_TOKEN_TTL,
+	});
 
 // hash refresh token before storing
 export const hashToken = (token) =>
-    crypto.createHash('sha256').update(token).digest('hex')
+	crypto.createHash("sha256").update(token).digest("hex");
