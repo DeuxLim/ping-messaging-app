7960b5d1aebbf421007030618fba4ed0478ddc8b
Deux Daniel Lim <limdeux27@gmail.com>
2026-01-22 10:44:25 +0800

    minor fix

--- client/src/components/chat/sidebar/SidebarHeaderSkeleton.jsx
+++ client/src/components/chat/sidebar/SidebarHeaderSkeleton.jsx
@@ -7,10 +7,10 @@ export default function SidebarHeader() {
             <header className="flex mb-1">
                 <div className="flex justify-between w-full items-center px-5 py-2">
                     <h1 className='text-2xl font-semibold rounded-md bg-gray-100 animate-pulse h-7 w-20'></h1>
-                    <Link to="/chats/new" className="flex justify-center items-center p-2 rounded-full bg-gray-100 animate-pulse">
+                    <div className="flex justify-center items-center p-2 rounded-full bg-gray-100 animate-pulse">
                         <div className="text-xl flex justify-center items-center size-4">
                         </div>
-                    </Link>
+                    </div>
                 </div>
             </header>
         </>
