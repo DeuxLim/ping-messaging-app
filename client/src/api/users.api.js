303f2d102df540f80e44b2237b29a0d600d373b1
Deux Daniel Lim <limdeux27@gmail.com>
2026-01-27 20:37:28 +0800

    user api

--- client/src/api/users.api.js
+++ client/src/api/users.api.js
@@ -1,5 +1,13 @@
 import { fetchAPI } from "./fetchAPI";
 
-export const getSuggestedUsers = () => {
+export const getSuggestedUsersAPI = () => {
 	return fetchAPI.get("/users/suggested");
+};
+
+export const updateProfileAPI = (data) => {
+	return fetchAPI.put("/users/update-profile", data);
+};
+
+export const updatePasswordAPI = (data) => {
+	return fetchAPI.put("/users/update-password", data);
 };
