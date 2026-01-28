56d1cc2aa8d99a302c2956a93d7688fb28330c5d
Deux Daniel Lim <limdeux27@gmail.com>
2026-01-28 20:20:54 +0800

    search chat and users api

--- client/src/api/chats.api.js
+++ client/src/api/chats.api.js
@@ -3,3 +3,7 @@ import { fetchAPI } from "./fetchAPI";
 export const getUserChatsAPI = () => {
 	return fetchAPI.get("/chats");
 };
+
+export const searchChatsAndUsersAPI = (searchQuery) => {
+	return fetchAPI.get(`/chats/search?q=${searchQuery}`);
+};
